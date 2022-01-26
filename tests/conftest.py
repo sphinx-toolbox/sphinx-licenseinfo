28d61962676c114cfc9ccfd73edad539ee2bb01e
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2022-01-26 22:09:18 +0000

    Close requests sessions when pytest session finished.

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,3 +4,8 @@ pytest_plugins = (
 		"sphinx_toolbox.testing",
 		"sphinx.testing.fixtures",
 		)
+
+
+def pytest_sessionfinish(session, exitstatus):
+	from sphinx_toolbox.utils import GITHUB_COM
+	GITHUB_COM.session.close()
